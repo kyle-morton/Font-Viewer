65b52042bfdd41b9dc1084afa5b7bfd42a36f007
Kyle Morton <mkmorton91@gmail.com>
2019-06-19 13:24:35 -0500

    created window, working on UI

---
+++ Font Viewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Font Viewer
+//
+//  Created by Kyle Morton on 6/19/19.
+//  Copyright © 2019 Morton-Development. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Font Viewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Font Viewer
+//
+//  Created by Kyle Morton on 6/19/19.
+//  Copyright © 2019 Morton-Development. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
