f3aa2443b7db0a43a0e69834171ba288e978b744
Kyle Morton <mkmorton91@gmail.com>
2019-06-20 13:07:26 -0500

    pulling in font list

--- Font Viewer/ViewController.swift
+++ Font Viewer/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: NSViewController {
     @IBOutlet weak var fontTypesPopup: NSPopUpButton!
     @IBOutlet weak var sampleLabel: NSTextField!
     
+    var selectedFontFamily: String?
+    
+    var fontFamilyMembers = [[Any]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,9 +30,62 @@ class ViewController: NSViewController {
         }
     }
     
+    override func viewWillAppear() {
+        super.viewWillAppear()
+        
+        setupUI()
+        populateFontFamilies()
+    }
+    
+    func setupUI () {
+        fontFamiliesPopup.removeAllItems()
+        fontTypesPopup.removeAllItems()
+        sampleLabel.stringValue = ""
+        sampleLabel.alignment = .center
+    }
+    
+    func populateFontFamilies () {
+        fontFamiliesPopup.removeAllItems()
+        fontFamiliesPopup.addItems(withTitles: NSFontManager.shared.availableFontFamilies)
+    }
+    
+    func updateFontTypesPopup () {
+        fontTypesPopup.removeAllItems()
+        
+        for member in fontFamilyMembers {
+            if let fontType = member[1] as? String { //REM: swift array indexes start at 1
+                fontTypesPopup.addItem(withTitle: fontType)
+            }
+        }
+    }
+    
     @IBAction func handleFontFamilySelection(_ sender: Any) {
         
-    }
+        if let fontFamily = fontFamiliesPopup.titleOfSelectedItem {
+            
+            selectedFontFamily = fontFamily
+            
+            if let members = NSFontManager.shared.availableMembers(ofFontFamily: selectedFontFamily!) {
+                
+                fontFamilyMembers.removeAll()
+                fontFamilyMembers = members
+                
+                /* Example Response from availableMembers() function -
+                 *
+                 * (("Times-Roman", "Roman", 5, 4),
+                 *    ("Times-Italic", "Italic", 6, 5),
+                 *    ("Times-Bold", "Bold", 9, 2),
+                 *    ("Times-BoldItalic", "Bold Italic", 9, 3)
+                 *    )
+                 *
+                */
+                
+                updateFontTypesPopup()
+            
+            }
+            
+        }
+     }
     
     @IBAction func handleFontTypeSelection(_ sender: Any) {
         
